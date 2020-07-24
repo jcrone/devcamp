170d831426b659dd42f0111c0ec520a5238c9ff2
Jamie Crone <jamiecrone@gmail.com>
2020-07-24 14:28:50 -0700

    data updates

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,17 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+
+  def toggle_status
+
+   if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+   end
+
+   redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+
 
   # GET /blogs
   # GET /blogs.json
