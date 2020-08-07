6716abaa3cbe673e3d174b60f8e90e6a6f201679
Jamie Crone <jamiecrone@gmail.com>
2020-08-07 08:32:33 -0700

    layout design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@ module ApplicationHelper
 
    def login_helper
      if !current_user.is_a?(GuestUser)
-      link_to "Edit", edit_user_registration_path
-       link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Edit", edit_user_registration_path, class: "nav-link"
+       link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link"
      else
-       (link_to "Sign Up", new_user_registration_path) +
-       "<br>".html_safe +
-       (link_to "Login", new_user_session_path)
+       (link_to "Sign Up", new_user_registration_path, class: "nav-link") +
+       " ".html_safe +
+       (link_to "Login", new_user_session_path, class: "nav-link")
      end
    end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,4 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap"
